caac138ff7d823ec0090563e7b40b220f862b56b
LAGGOUNE-Walid <walidlaggoune159@gmail.com>
2022-07-11 19:30:00 +0100

    added voyager

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return phpinfo();
     return view('welcome');
+});
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
 });
