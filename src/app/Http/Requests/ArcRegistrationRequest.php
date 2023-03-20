8445c1f93a64094b89fb4470a9e9ccf1571a0aeb
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2023-03-20 17:19:12 +0100

    Fixed bug

--- src/app/Http/Requests/ArcRegistrationRequest.php
+++ src/app/Http/Requests/ArcRegistrationRequest.php
@@ -35,7 +35,7 @@ class ArcRegistrationRequest extends FormRequest
             'motivation' => ['required'],
             'password' => ['required'],
             'tshirt' => ['required'],
-            'id_card' => ['required', 'mimetypes:image/jpeg,image/jpg,image/png|max:15042']
+            'id_card' => ['required', 'mimetypes:image/jpeg,image/jpg,image/png','max:15042']
         ];
     }
 }
